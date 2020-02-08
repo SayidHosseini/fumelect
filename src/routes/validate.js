25f21cd65a060dd065b95f7dcf233329024c6e95
SayidHosseini <sayidhosseini@hotmail.com>
2020-02-08 11:00:49 +0330

    Add extract middleware, Modify routes accordingly

--- src/routes/validate.js
+++ src/routes/validate.js
@@ -1,28 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
-const User = require('../models/user');
-const jwt = require('../services/jwt');
 const token = require('../middlewares/token');
 const rm = require('../static/responseMessages');
-const sn = require('../static/names');
 
 router.get('/token', token.validate, (req, res, next) => {
-    const token = req.get(sn.authorizationName).split(' ')[1]; // Extract the token from Bearer
-    User.getUserByEmail(jwt.decode(token).payload.email).then((user) => {
-        if (!user) {
-            return res.deliver(rm.sessionInvalid);
-        }
-
-        const body = {
-            [sn.message]: rm.loggedIn.msg.message,
-            [sn.userID]: user._id,
-            [sn.email]: user.email
-        };
-        return res.deliver(rm.loggedIn, body);
-    }).catch((err) => {
-        return next(err);
-    });
+    return res.deliver(rm.loggedIn);
 });
 
 module.exports = router;
--- src/static/names.js
+++ src/static/names.js
@@ -1,13 +1,8 @@
 module.exports = {
-    role: "role",
     superAdminRole: "super",
     adminRole: "admin",
     userRole: "user",
     guestRole: "guest",
-    email: "email",
-    user: "user",
-    userID: "userID",
-    message: "message",
     production: "production",
     dbReady: "DatabaseReady",
     duplicateError: 11000,
