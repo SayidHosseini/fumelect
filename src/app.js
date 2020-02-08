92e8cb457a24ac739ad08949cd9dde496bb19933
SayidHosseini <sayidhosseini@hotmail.com>
2020-02-08 17:53:14 +0330

    Modify errors middleware name to error

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const db = require('./scripts/database');
 const reply = require('./middlewares/reply');
-const errors = require('./middlewares/errors');
+const error = require('./middlewares/error');
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/users');
 const validateRouter = require('./routes/validate');
@@ -18,7 +18,7 @@ app.use('/authentiq/v1/', indexRouter);
 app.use('/authentiq/v1/validate', validateRouter);
 app.use('/authentiq/v1/user', userRouter);
 
-app.use(errors.notFound);
-app.use(errors.unknown);
+app.use(error.notFound);
+app.use(error.unknown);
 
 module.exports = app;
